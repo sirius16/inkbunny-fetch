6e0fd5ce03e27f981863096de790cf5179fb466c
sirius16 <amozu16@gmail.com>
2015-08-01 06:33:17 -0400

    Update inkbunny-fetch.js

--- inkbunny-fetch.js
+++ inkbunny-fetch.js
@@ -1,26 +1,36 @@
 var URL = window.location.href;
 end = /page=\d+/;
+
+
 $.ajaxSetup({
-	async: false
+	async : false
 });
-hi=[];
+
+
+hi = [];
+
+
 var pages = $("[title*='final page']")[0].href.match(/\d+/g).last();
+
+
 for (i = 1; i < eval(pages) + 1; i++) {
 	jQuery.ajax({
-		url: URL.replace(end, "page=" + i),
-		success: function (result) {
+		url : URL.replace(end, "page=" + i),
+		success : function (result) {
 			var $result = eval($(result));
 			$result.find("a[href*=submissionview]").map(function (i, j) {
 				return hi.push(j.href);
 			});
 		},
-		async: false,
-		script: true
+		async : false,
+		script : true
 	});
 }
-var obj= $('<textarea />');
+
+
+var obj = $('<textarea />');
 $("body").append(obj);
-var hello=hi.join("\n");
+var hello = hi.join("\n");
 console.clear();
 console.log(hello);
 obj.select().focus();

06834627c7a599c93be2ab09485ba325dc1cc2ef
sirius16 <amozu16@gmail.com>
2016-08-26 02:11:51 -0400

    Z316Z

--- inkbunny-fetch.js
+++ inkbunny-fetch.js
@@ -7,22 +7,17 @@ $j.ajaxSetup({
 
 hi = [];
 
-var pages = $j("[title*='final page']")[0].href.match(/\d+/g).last();
+var pages = $j("span").filter((i,j)=>j.innerText.match(/page \d+ of \d+/)).text().match(/\d+/g).last();
 
-for (i = 1; i < eval(pages) + 1; i++) {
-	$j.ajax({
-		url : URL.replace(end, "page=" + i),
-		success : function (result) {
-			var $result = eval($j(result));
-			$result.find("a[href*=submissionview]").map(function (i, j) {
-				console.log(hi.length);
-				return hi.push(j.href);
-			});
-		},
-		async : false,
-		script : true
-	});
-}
+for (i = 1; i < parseInt(pages) + 1; i++) {
+	$j.get(URL.replace(end, "page=" + i), function (result) {
+		var $result = $j(result);
+		$result.find(".widget_thumbnailHugeCompleteFromSubmission  a[href*=submissionview]").map(function (i, j) {
+			console.log(hi.length);
+			return hi.push(j.href);
+		});
+	})
+};
 hello = hi.reverse().join("%0A");
 date = new Date();
 date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
@@ -30,5 +25,5 @@ time = date.toJSON().replace(/T|\..*/g, " ").replace(/:/g, "-").trim();
 var link = document.createElement('a');
 link.download = "Inkbunny " + time + ".txt";
 link.href = 'data:,' + hello;
-link.click()
+link.click();
 alert(hi.length);
